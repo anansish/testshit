4d6a6bdafe67abc4739877d947aaa42e07c8929f
anansish <anansishaha@gmail.com>
2022-09-17 14:34:41 +0300

    move max size in pixels to separate variable

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -244,7 +244,7 @@ class DDIMSampler(object):
             index = total_steps - i - 1
             ts = torch.full((x_latent.shape[0],), step, device=x_latent.device, dtype=torch.long)
 
-            if z_mask is not None and i <= total_steps - 2:
+            if z_mask is not None and i <= total_steps:
                 assert x0 is not None
                 img_orig = self.model.q_sample(x0, ts)  # TODO: deterministic forward pass?
                 mask_inv = 1. - z_mask
